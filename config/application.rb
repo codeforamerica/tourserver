3385ef965c6bb690afb57587d4bac0282043bb5a
Dan Avery <dan.github@danavery.com>
2013-04-16 12:40:16 -0700

    Possible fix for Heroku--not doing assets:precompile

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module TourServer
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #adding to make Heroku happy
+    config.assets.initialize_on_precompile = false
   end
 end
